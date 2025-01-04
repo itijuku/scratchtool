ee3258b39452220a69b54162f5985f288d2807e5
itijiku <kanjukuitijuku@gmail.com>
2025-01-04 11:52:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,23 @@ setup(
     name='scratchtool',
     version='1.0.0',
     author="itijiku",
-    author_email = "kanjukuitijuku@gmail.com",
-    description="scratch's package",
+    author_email="kanjukuitijuku@gmail.com",
+    description="A package for working with Scratch data.",
+    url="https://github.com/itijuku/scratchtool",
+    packages=find_packages(),
     install_requires=[
-        'requests',
-        'beautifulsoup4',
+        'requests>=2.31.0',
+        'beautifulsoup4>=4.12.3',
     ],
     entry_points={
-    'console_scripts': [
-        'scratchtool=scratchtool.main:main',
-    ],
+        'console_scripts': [
+            'scratchtool=scratchtool.main:main',
+        ],
     },
-    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
