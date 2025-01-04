308474d0adb38aba32fe3ca28b78d870a3e25872
itijiku <kanjukuitijuku@gmail.com>
2025-01-04 17:28:45 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scratchtool',
-    version='1.0.0',
+    version='1.0.2',
     author="itijiku",
     author_email="kanjukuitijuku@gmail.com",
     description="A package for working with Scratch data.",
